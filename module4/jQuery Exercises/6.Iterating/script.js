30461924d44cc8e6b6676e8e6501c1acd13e491b
AndyGoldman <andy.goldman@hotmail.com>
2014-05-07 20:47:29 -0600

    Started Exercise 7

--- module4/jQuery Exercises/6.Iterating/script.js
+++ module4/jQuery Exercises/6.Iterating/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-		$('.sandbox ul li:odd').css('color','black');
+		$('.sandbox ul li:even').css('color','black');
 			console.log('Step 1');
-		$('.sandbox ul li:even').css('color','gray');
+		$('.sandbox ul li:odd').css('color','gray');
 			console.log('Step 2');
 });
