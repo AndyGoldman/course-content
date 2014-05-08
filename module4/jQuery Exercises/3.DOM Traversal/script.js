cf0f73ca7cbf8b6a47e1d34f26f37e6be6692269
AndyGoldman <andy.goldman@hotmail.com>
2014-05-07 18:46:26 -0600

    Added js for exercises 3 and 4
    Entered Jake's js for both exercises to study later

--- module4/jQuery Exercises/3.DOM Traversal/script.js
+++ module4/jQuery Exercises/3.DOM Traversal/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
-  $(".sample").find("span").last().replaceWith($(".sample").find("span").first().text());
-  $(".sample").find("p").last().replaceWith($(".sample").find("span").first().text());
-});	
+ //* $(".sample").find("span").last().replaceWith($(".sample").find("span").first().text());
+  //*$(".sample").find("p").last().replaceWith($(".sample").find("span").first().text());
+//*});	
+
+	var word = $('.sample').children('.firstParagraph').children('.targetText').text();
+		console.log(word);
+	$('.sample').children('.firstParagraph').children('.nextText').text(word);
+	$('.sample').children('.emptyText').text(word);
+});
