1fbef5e0ac76b98d6a2abbb70d5630212d346f83
AndyGoldman <andy.goldman@hotmail.com>
2014-05-07 21:46:24 -0600

    Completed Exercise 7

--- module4/jQuery Exercises/7.Clone,Add,Count/script.js
+++ module4/jQuery Exercises/7.Clone,Add,Count/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	
+	$('button.add-item').click(function(){
+		var repeat = $('#numberToAdd').val();
+		for(var i = 1; i<=repeat; i++){
+    $('.template li').clone().text('You rock!').appendTo('.sample-list');
+			console.log('Step 1');
+		$('.count').text(i);	
+			};
+	});
 });
