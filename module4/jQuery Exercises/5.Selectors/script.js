bf0e1519210c356d854cef3ccd214595992b52db
AndyGoldman <andy.goldman@hotmail.com>
2014-05-07 19:12:53 -0600

    Added js to Exercise 5

--- module4/jQuery Exercises/5.Selectors/script.js
+++ module4/jQuery Exercises/5.Selectors/script.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+		$('.sandbox p:nth-child(2)').css('color','red');
+			console.log('Step 1');
+		$('.sandbox p:nth-child(1)').css('background-color','red');
+			console.log('Step 2');
+		$('.sandbox p:nth-child(3)').remove();
+			console.log('Step 3');
+});
+		//*$('div.sandbox p').css({'border-width': '2px','border-style': 'solid', 'border-color': 'rgba(255,255,128,0.8)'});
+		//*$('.sample').children('.firstParagraph').children('.nextText').text(word);
